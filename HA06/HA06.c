f589803f900f810df324117c6b3fe857ffd0e108
der-tim-01 <64968142+der-tim-01@users.noreply.github.com>
2020-06-14 17:31:32 +0200

    Create HA06.c

---
+++ HA06/HA06.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+//HAO6 Jan Brudermüller und Tim Schierle
+
+int main(){
+
+printf("Multiple Choice\n\n");
+
+printf("Antwort a): 2\n");
+printf("Antwort b): 3\n");
+printf("Antwort c): 3\n");
+printf("Antwort d): 3\n");
+printf("Antwort e): 3\n");
+printf("Antwort f): 2\n");
+
+printf("\nBasics\n\n");
+int i = 533;
+while( i > -12){
+    printf("%d textbackslash \n", i);
+    i -=7;
+}
+
+int arr[5] = {1,3,5,7,9};
+int* ptr = arr + 2;
+ptr[1] = 0;
+
+printf("Pointer nach Ausfuehrung: {%d,%d,%d,%d,%d}\n",arr[0],arr[1],arr[2],arr[3],arr[4]);
+
+
+}
